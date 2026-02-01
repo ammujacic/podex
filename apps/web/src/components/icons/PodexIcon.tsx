3027307e1b018f22837b03e1424e57369fd7d06b
Amir Mujacic <amir.mujacic@sentry.io>
2026-02-01 18:17:20 +0100

    Fix the Icon

--- apps/web/src/components/icons/PodexIcon.tsx
+++ apps/web/src/components/icons/PodexIcon.tsx
@@ -16,12 +16,12 @@ export interface PodexIconProps extends SVGProps<SVGSVGElement> {
 export const PodexIcon = forwardRef<SVGSVGElement, PodexIconProps>(
   ({ size = 24, className, variant = 'full', ...props }, ref) => {
     if (variant === 'mark') {
-      // Single-color P{x} mark using currentColor
+      // Single-color P{x} mark using currentColor (matches podex.svg)
       return (
         <svg
           ref={ref}
           role="img"
-          viewBox="0 0 32 32"
+          viewBox="0 0 24 24"
           xmlns="http://www.w3.org/2000/svg"
           width={size}
           height={size}
@@ -34,29 +34,29 @@ export const PodexIcon = forwardRef<SVGSVGElement, PodexIconProps>(
           <path
             fillRule="evenodd"
             clipRule="evenodd"
-            d="M7 5H16C19.866 5 23 8.13401 23 12C23 15.866 19.866 19 16 19H12V25H7V5ZM12 9V15H15.5C17.157 15 18.5 13.657 18.5 12C18.5 10.343 17.157 9 15.5 9H12Z"
+            d="M4 3H12C15.3137 3 18 5.68629 18 9C18 12.3137 15.3137 15 12 15H8V19H4V3ZM8 6V12H11.5C13.1569 12 14.5 10.6569 14.5 9C14.5 7.34315 13.1569 6 11.5 6H8Z"
           />
           {/* Left curly brace */}
           <path
-            d="M15 19 Q13.5 19 13.5 20.5 Q13.5 22 12.5 22 Q13.5 22 13.5 23.5 Q13.5 25 15 25"
+            d="M11.5 15 Q10.5 15 10.5 16 Q10.5 17 10 17 Q10.5 17 10.5 18 Q10.5 19 11.5 19"
             stroke="currentColor"
-            strokeWidth="1.2"
+            strokeWidth="1"
             strokeLinecap="round"
             fill="none"
           />
           {/* Small x */}
           <path
-            d="M17.5 19.5L21.5 23.5M21.5 19.5L17.5 23.5"
+            d="M13.5 15.5L17.5 19.5M17.5 15.5L13.5 19.5"
             stroke="currentColor"
-            strokeWidth="1.5"
+            strokeWidth="1.2"
             strokeLinecap="round"
             fill="none"
           />
           {/* Right curly brace */}
           <path
-            d="M24 19 Q25.5 19 25.5 20.5 Q25.5 22 26.5 22 Q25.5 22 25.5 23.5 Q25.5 25 24 25"
+            d="M19.5 15 Q20.5 15 20.5 16 Q20.5 17 21 17 Q20.5 17 20.5 18 Q20.5 19 19.5 19"
             stroke="currentColor"
-            strokeWidth="1.2"
+            strokeWidth="1"
             strokeLinecap="round"
             fill="none"
           />
@@ -64,12 +64,12 @@ export const PodexIcon = forwardRef<SVGSVGElement, PodexIconProps>(
       );
     }
 
-    // Full favicon style with dark background
+    // Full favicon style with dark background (matches podex.svg)
     return (
       <svg
         ref={ref}
         role="img"
-        viewBox="0 0 32 32"
+        viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg"
         width={size}
         height={size}
@@ -78,37 +78,38 @@ export const PodexIcon = forwardRef<SVGSVGElement, PodexIconProps>(
       >
         <title>Podex</title>
         {/* Dark background */}
-        <rect width="32" height="32" rx="6" fill="#07070a" />
+        <rect width="24" height="24" rx="4.5" fill="#07070a" />
         {/* Large P */}
         <path
-          d="M7 5H16C19.866 5 23 8.13401 23 12C23 15.866 19.866 19 16 19H12V25H7V5Z"
+          d="M4 3H12C15.3137 3 18 5.68629 18 9C18 12.3137 15.3137 15 12 15H8V19H4V3Z"
           fill="white"
+          opacity="0.9"
         />
         {/* Inner hole (same as background) */}
         <path
-          d="M12 9H15.5C17.157 9 18.5 10.343 18.5 12C18.5 13.657 17.157 15 15.5 15H12V9Z"
+          d="M8 6H11.5C13.1569 6 14.5 7.34315 14.5 9C14.5 10.6569 13.1569 12 11.5 12H8V6Z"
           fill="#07070a"
         />
         {/* Left curly brace */}
         <path
-          d="M15 19 Q13.5 19 13.5 20.5 Q13.5 22 12.5 22 Q13.5 22 13.5 23.5 Q13.5 25 15 25"
+          d="M11.5 15 Q10.5 15 10.5 16 Q10.5 17 10 17 Q10.5 17 10.5 18 Q10.5 19 11.5 19"
           stroke="white"
-          strokeWidth="1.2"
+          strokeWidth="1"
           strokeLinecap="round"
           fill="none"
         />
         {/* Small x */}
         <path
-          d="M17.5 19.5L21.5 23.5M21.5 19.5L17.5 23.5"
+          d="M13.5 15.5L17.5 19.5M17.5 15.5L13.5 19.5"
           stroke="white"
-          strokeWidth="1.5"
+          strokeWidth="1.2"
           strokeLinecap="round"
         />
         {/* Right curly brace */}
         <path
-          d="M24 19 Q25.5 19 25.5 20.5 Q25.5 22 26.5 22 Q25.5 22 25.5 23.5 Q25.5 25 24 25"
+          d="M19.5 15 Q20.5 15 20.5 16 Q20.5 17 21 17 Q20.5 17 20.5 18 Q20.5 19 19.5 19"
           stroke="white"
-          strokeWidth="1.2"
+          strokeWidth="1"
           strokeLinecap="round"
           fill="none"
         />
